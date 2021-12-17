eeb5a681687342f0232059282f60bd4c3ae379c4
Matthew John <matthew@dockstudios.co.uk>
2021-12-17 13:50:55 +0000

    Attempt to create background tile function and iterate over maintile map to generate color palette -e Original/build commit ID: 261da3861c77dac09f98f510b603988bd58ba4a3

--- main.c
+++ main.c
@@ -12,6 +12,42 @@
 #include "main_map.c"
 #include "main_map_palette.c"
 
+unsigned char *background_tile_map;
+unsigned char *background_tiles;
+unsigned char *background_tile_palette;
+unsigned int background_palette_itx_x;
+unsigned int background_palette_itx_y;
+
+void set_background_tiles()
+{
+    set_bkg_tiles(0, 0, mainmapWidth, mainmapHeight, background_tile_map);
+    VBK_REG = 0;
+    set_bkg_data(0, 8, background_tiles);
+
+
+    VBK_REG = 1;
+    for (background_palette_itx_x = 0; background_palette_itx_x < mainmapWidth; background_palette_itx_x ++)
+    {
+        for (background_palette_itx_y = 0; background_palette_itx_y < mainmapHeight; background_palette_itx_y ++)
+        {
+            // Set palette data in VBK_REG1 for tile
+            set_bkg_tiles(
+                background_palette_itx_x, 
+                background_palette_itx_y,
+                1, 1,  // Only setting 1 tile
+                background_tile_palette[  // From the palette map
+                    // Lookup tile from background tile map
+                    background_tile_map[
+                        background_palette_itx_x + (background_palette_itx_y * mainmapWidth)  // Calculate index based on X, y index
+                    ]
+                ]
+            );
+        }
+    }
+
+    // Reset VKG_REG to original value
+    VBK_REG = 0;
+}
 
 void main()
 {
@@ -20,14 +56,19 @@ void main()
         waitpad(J_START);  // other keys are J_A, J_UP, J_SELECT, etc.
         printf("\nIsn't it easy!");
   */      
-        set_bkg_data(0, 11, mainmaptiles);
 
-      set_bkg_palette(0, 8, &bgpal);
+    // Load color palette
+    set_bkg_palette(0, 8, &bgpal);
 
-        set_bkg_tiles(0, 0, mainmapWidth, mainmapHeight, mainmap);
- wait_vbl_done();
-        SHOW_BKG;
-        DISPLAY_ON;
+    // Load background tiles
+    background_tile_map = &mainmap;
+    background_tiles = &mainmaptiles;
+    background_tile_palette = &mainmaptilesCGB;
+    set_background_tiles();
+
+    wait_vbl_done();
+    SHOW_BKG;
+    DISPLAY_ON;
         
         while(1) {
                 wait_vbl_done();
