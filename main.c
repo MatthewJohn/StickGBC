88300ddcf47858f2c5a6ae913c89847114ffc51c
Matthew John <matthew@dockstudios.co.uk>
2021-12-17 18:49:33 +0000

    Add current buffer display position.
    Have to create variable to store max_x/max_y, as performing arithmatic in
    the definition of the for loop causes weird side affects.
    -e 
    Original/build commit ID: 16cbc9f08f87e410e4ef83e1472cbaf0f7e6b3f0

--- main.c
+++ main.c
@@ -16,6 +16,7 @@
 #define BACKGROUND_BUFFER_SIZE_X 0x1FU
 #define BACKGROUND_BUFFER_SIZE_Y 0x1FU
 
+
 unsigned char *background_tile_map;
 unsigned char *background_tiles;
 unsigned char *background_tile_palette;
@@ -23,16 +24,37 @@ unsigned int background_palette_itx_x;
 unsigned int background_palette_itx_y;
 unsigned char TILE_TRANSFER;
 
+unsigned int FRAME_BUFFER_TILE_POS_X;
+unsigned int FRAME_BUFFER_TILE_POS_Y;
+unsigned int FRAME_BUFFER_TILE_MAX_X;
+unsigned int FRAME_BUFFER_TILE_MAX_Y;
+
+
+//unsigned long MAIN_MAP_VERTICAL_FLIP_TILES[] = {
+//    0xff
+//};
+
+void init_map_variables()
+{
+    FRAME_BUFFER_TILE_POS_X = 0;
+    FRAME_BUFFER_TILE_POS_Y = 0;
+}
 
 void set_background_tiles()
 {
     //set_bkg_tiles(0, 0, mainmapWidth, mainmapHeight, background_tile_map);
     VBK_REG = 0;
     set_bkg_data(0, 8, background_tiles);
+    FRAME_BUFFER_TILE_MAX_X = BACKGROUND_BUFFER_SIZE_X + FRAME_BUFFER_TILE_POS_X;
+    FRAME_BUFFER_TILE_MAX_Y = BACKGROUND_BUFFER_SIZE_Y + FRAME_BUFFER_TILE_POS_Y;
 
-    for (background_palette_itx_x = 0; background_palette_itx_x < BACKGROUND_BUFFER_SIZE_X; background_palette_itx_x ++)
+    for (background_palette_itx_x = FRAME_BUFFER_TILE_POS_X;
+           background_palette_itx_x != FRAME_BUFFER_TILE_MAX_X;
+           background_palette_itx_x ++)
     {
-        for (background_palette_itx_y = 0; background_palette_itx_y < BACKGROUND_BUFFER_SIZE_Y; background_palette_itx_y ++)
+        for (background_palette_itx_y = FRAME_BUFFER_TILE_POS_Y;
+               background_palette_itx_y != FRAME_BUFFER_TILE_MAX_Y;
+               background_palette_itx_y ++)
         {
             // Map data is 2 bytes per tile.
             // First byte's first 7 bits are tile number
@@ -77,7 +99,7 @@ void main()
 //        printf("\nPress Start");
 //        waitpad(J_START);  // other keys are J_A, J_UP, J_SELECT, etc.
 //        printf("\nIsn't it easy!");
-        
+    init_map_variables();
 
     // Load color palette
     set_bkg_palette(0, 8, &bgpal);
