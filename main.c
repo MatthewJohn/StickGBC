939f346599c4c6408a34aa420a574cc3100c99d5
Matthew John <matthew@dockstudios.co.uk>
2021-12-17 12:52:15 +0000

    Get color palettes working.
    Main requirement was the change to make.bat
    -e 
    Original/build commit ID: 9c0f0487428742633a5c5b95ceac3a0edf2f729e

--- main.c
+++ main.c
@@ -53,15 +53,17 @@ void main()
         waitpad(J_START);  // other keys are J_A, J_UP, J_SELECT, etc.
         printf("\nIsn't it easy!");
   */      
-        set_bkg_palette(0, 8, &bgpal);
         set_bkg_data(0, 11, mainmaptiles);
 
+      set_bkg_palette(0, 1, &bgpal);
+
         set_bkg_tiles(0, 0, mainmapWidth, mainmapHeight, mainmap);
- 
+ wait_vbl_done();
         SHOW_BKG;
         DISPLAY_ON;
         
         while(1) {
+                wait_vbl_done();
                 scroll_bkg(1, 0);
                 delay(100);
         }
--- make.bat
+++ make.bat
@@ -1,3 +1,3 @@
 ..\..\bin\lcc -Wa-l -c -o main.o main.c
-..\..\bin\lcc -Wl-m -o main.gb main.o
+..\..\bin\lcc -Wl-m  -Wl-yp0x143=0x80 -o main.gb main.o
 
