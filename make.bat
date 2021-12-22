dadecb7ed09770fd478e9e5ad8922a96dfa21a29
Matthew John <matthew@dockstudios.co.uk>
2021-12-22 12:12:09 +0000

    Move background to seperate rom bank to avoid overflow -e Original/build commit ID: d7757d445e565e14fbd4d56a5d81de1d2f85c176

--- make.bat
+++ make.bat
@@ -1,5 +1,6 @@
 
-rm main.o main.gb
+rm main.o main_map.o main.gb
 ..\..\bin\lcc -Wa-l -c -o main.o main.c
-..\..\bin\lcc -Wl-m -Wl-yp0x143=0x80 -o main.gb main.o
+..\..\bin\lcc -Wf-bo1 -c -o main_map.o main_map.c
+..\..\bin\lcc -Wl-yt2 -Wl-yo16 -Wl-ya4 -Wl-m -Wl-yp0x143=0x80 -o main.gb main.o main_map.o
 
