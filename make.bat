b9215af14b73c59e32f7fb6ad1b7f36cc8314edf
Matthew John <matthew@dockstudios.co.uk>
2021-12-22 22:26:02 +0000

    Issue #5 Move boundary array to bank2 and combine with main map -e Original/build commit ID: 5e61f7eb8da9a4d410636e4b4da2e851677070aa

--- make.bat
+++ make.bat
@@ -1,7 +1,5 @@
 
-rm main.o main_map.o main.gb
+rm *.o main.gb
 ..\..\bin\lcc -Wa-l -c -o main.o main.c
-..\..\bin\lcc -Wf-bo1 -c -o main_map.o main_map.c
-..\..\bin\lcc -Wf-bo2 -c -o main_map_boundaries.o main_map_boundaries.c
-..\..\bin\lcc -Wl-yt2 -Wl-yo16 -Wl-ya4 -Wl-m -Wl-yp0x143=0x80 -o main.gb main.o main_map.o main_map_boundaries.o
-
+..\..\bin\lcc -Wf-bo1 -c -o bank2.o bank2.c
+..\..\bin\lcc -Wl-yt2 -Wl-yo16 -Wl-ya4 -Wl-m -Wl-yp0x143=0x80 -o main.gb main.o bank2.o
