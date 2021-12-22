a53922ae737e027287ea330a8541f725d174fe10
Matthew John <matthew@dockstudios.co.uk>
2021-12-22 21:27:34 +0000

    Issue #5 Move main_map_boundaries to seperate rom bank -e Original/build commit ID: 253c8746d04275b6d95eff7badb675e925234c9b

---
+++ main_map_boundaries.h
@@ -0,0 +1,10 @@
+/*
+ * StickGBC by Matt Comben is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+ * To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by-nc-nd/4.0/.
+ */
+
+
+#include "gb/gb.h"
+
+extern const UBYTE MAIN_MAP_BOUNDARIES[];
--- make.bat
+++ make.bat
@@ -2,5 +2,6 @@
 rm main.o main_map.o main.gb
 ..\..\bin\lcc -Wa-l -c -o main.o main.c
 ..\..\bin\lcc -Wf-bo1 -c -o main_map.o main_map.c
-..\..\bin\lcc -Wl-yt2 -Wl-yo16 -Wl-ya4 -Wl-m -Wl-yp0x143=0x80 -o main.gb main.o main_map.o
+..\..\bin\lcc -Wf-bo1 -c -o main_map_boundaries.o main_map_boundaries.c
+..\..\bin\lcc -Wl-yt2 -Wl-yo16 -Wl-ya4 -Wl-m -Wl-yp0x143=0x80 -o main.gb main.o main_map.o main_map_boundaries.o
 
