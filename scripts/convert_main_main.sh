de6c4ddb7893bb038ebe0dc43401a2a3d9d91bf6
Matthew John <matthew@dockstudios.co.uk>
2021-12-25 05:51:00 +0000

    Issue #13 Add loading of new building menu tiles -e Original/build commit ID: 441bcca8253d2ad887069e1d791cba36dd6b2f3f

--- scripts/convert_main_main.sh
+++ scripts/convert_main_main.sh
@@ -7,9 +7,7 @@
 #cat main_map.c | sed -z 's/,\n  /,/g' | sed -E 's/0x([0-9A-F][0-9A-F]),0x([0-9A-F])[0-9A-F],0x([0-9A-F])([0-9A-F]),0x([0-9A-F])[0-9A-F]/QQ\1,QQ\2\3,QQ\4\5/g' | sed 's/QQ/0x/g' > main_map.c
 
 # Convert main_map.c to use const
-sed -i 's/^unsigned char mainmap/const unsigned char mainmap/g' main_map.c
-sed -i 's/^extern unsigned char mainmap/extern const unsigned char mainmap/g' main_map.h
+sed -i 's/^unsigned char mainmap/const unsigned char/g' main_map.c main_map_tileset.c building_menu_tiles.c
+sed -i 's/^extern unsigned char/extern const unsigned char/g' main_map.h main_map_tileset.h building_menu_tiles.h
 
-sed -i 's/^extern unsigned char/extern const unsigned char/g' main_map_tileset.h
-sed -i 's/^unsigned char/const unsigned char/g' main_map_tileset.c
 
