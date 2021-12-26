de68de8afd6754bf75c4d1ae25ea33346bc24268
Matthew John <matthew@dockstudios.co.uk>
2021-12-26 13:58:36 +0000

    Remove debug -e Original/build commit ID: 513bfaf0bc9668a15c47a5ffc93e99ae6d261e78

--- main.c
+++ main.c
@@ -825,10 +825,6 @@ void update_state()
 
 void main()
 {
-//        printf("Welcome to GAMEBOY\nProgramming");
-//        printf("\nPress Start");
-//        waitpad(J_START);  // other keys are J_A, J_UP, J_SELECT, etc.
-//        printf("\nIsn't it easy!");
     debug_address = 0xFFFA;
 
     DISPLAY_OFF;
