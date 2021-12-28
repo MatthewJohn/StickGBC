d5bd7b9476667646a57592defb4be558cdcc21d7
Matthew John <matthew@dockstudios.co.uk>
2021-12-28 15:50:43 +0000

    Issue #28 Add functionality for purchase food and work
    Add HP, max HP and balance to game state.
    Add setting of initial values to setup_globals.
    Add functions to purchase food and to do work.
    Add use of these functions in logic when selecting menu item.
    -e 
    Original/build commit ID: 9e984b371f0624ca200443f702893dbc40009e6c

--- game_state.c
+++ game_state.c
@@ -10,13 +10,16 @@
 
 // Game state and constants for game state
 
+#define S_HOURS_PER_DAY 24U
+// 8AM
+#define S_HOUR_WAKEUP_NORMAL 0x08U
+
 // Buildings
 #define S_B_NO_BUILDING 0x00U
 #define S_B_MAIN_MENU 0x01U
 #define S_B_HOUSE 0x02U
 #define S_B_RESTAURANT 0x04U
 
-#define S_HOUR_WAKEUP_NORMAL 0x04U;
 
 
 typedef struct {
@@ -27,4 +30,13 @@ typedef struct {
     
     // Current hour of day (out of 12).
     UINT8 hour;
+    
+    // Max HP the user can have
+    UINT8 max_hp;
+    
+    // Current amount of HP the user has
+    UINT8 hp;
+    
+    // Money the user has.
+    unsigned long balance;
 } game_state_t;
