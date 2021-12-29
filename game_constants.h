ec1d51b583724843506ec1d0b81ddde90c8d23f8
Matthew John <matthew@dockstudios.co.uk>
2021-12-29 08:00:00 +0000

    Move sleep times to game constants header file. Revert starting day -e Original/build commit ID: 6001fb6b3bf034f8cdc43f890102958e993853b0

---
+++ game_constants.h
@@ -0,0 +1,11 @@
+/*
+ * StickGBC by Matt Comben is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+ * To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by-nc-nd/4.0/.
+ */
+
+
+// Delays and timings
+#define DELAY_PURCHASE_ITEM 700U
+#define DELAY_SLEEP 1000U
+#define DELAY_MENU_ITEM_MOVE 100U
