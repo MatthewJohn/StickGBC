dc02beae6d10ee8fddcce889f70d4c702018fca5
Matthew John <matthew@dockstudios.co.uk>
2022-01-01 22:42:09 +0000

    Issue #18 Add transition to load in tile data for LNI.
    This shares boundary with shop so that they share the same palette
    -e 
    Original/build commit ID: 8ef6eb39cf881768f55f47854955356ad390ddb0

--- screen_state.c
+++ screen_state.c
@@ -10,13 +10,17 @@
 #define SC_SHOP 0x04U
 #define SC_PAWN 0x08U
 #define SC_UNIVERSITY 0x10U
+#define SC_NLI 0x20U
 
 // Show/remove left-hand buildings when screen position passes this point
 #define SC_HOUSE_TRANSITION_X 0x12U
 #define SC_RESTAURANT_TRANSITION_Y 0x7U
-#define SC_SHOP_TRANSITION_Y 0x15U
+#define SC_SHOP_NLI_TRANSITION_Y 0x16U
 #define SC_PAWN_TRANSITION_Y 0x20U
 #define SC_UNIVERSITY_TRANSITION_X 0x1DU
+//#define SC_NLI_TRANSITION_X_MIN 0x16U
+#define SC_NLI_TRANSITION_X_MIN 0x16U
+#define SC_NLI_TRANSITION_X_MAX 0x46U
 
 // Sprites
 #define SC_SPRITE_SKATER 0x01U
