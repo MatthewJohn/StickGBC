a9d3f0f923982fb50b109a3e549bb729a4cba74d
Matthew John <matthew@dockstudios.co.uk>
2022-01-03 06:54:31 +0000

    Issue #67 Fix initial HP constant to be 0xF (15) -e Original/build commit ID: f8269867d33eca0dc6b8176946fb574cd2580640

--- game_state.c
+++ game_state.c
@@ -15,7 +15,7 @@
 #define S_HOUR_WAKEUP_NORMAL 0x08U
 #define S_HOUR_CAFFEINE_BONUS 0x04U
 
-#define S_INITIAL_BASE_HP 0xEU
+#define S_INITIAL_BASE_HP 0xFU
 
 // Buildings
 #define S_B_NO_BUILDING 0x00U
