c7cbd9d6f1cc6bef940ac629846676b83b36f121
Matthew John <matthew@dockstudios.co.uk>
2022-01-03 07:52:16 +0000

    Issue #21 Move move_ai_sprite to bank 5.
    Create new C/H files to store function
    -e 
    Original/build commit ID: ca1703a42a8a900f73d19466cf7d3d8aaee07c4c

---
+++ sprite.h
@@ -0,0 +1,10 @@
+/*
+ * StickGBC by Matt Comben is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+ * To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by-nc-nd/4.0/.
+ */
+
+
+#include "game_state.c"
+
+void move_ai_sprite(ai_sprite* sprite_to_move);
