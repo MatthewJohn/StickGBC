0d27bd6b224ec2df43a18b1a9f52cd6766047a86
Matthew John <matthew@dockstudios.co.uk>
2022-01-05 21:46:56 +0000

    Issue #72 Fix definition of show_signed_number function -e Original/build commit ID: 39e2b2e3ad904c5f28bda8f1c3546e9544bbb04a

--- window.h
+++ window.h
@@ -6,6 +6,6 @@
 
 
 void show_number(UINT8 start_x, UINT8 start_y, UINT8 max_digits, unsigned int value);
-void show_signed(UINT8 start_x, UINT8 start_y, UINT8 max_digits, INT8 value);
+void show_signed_number(UINT8 start_x, UINT8 start_y, UINT8 max_digits, INT8 value);
 void update_window(game_state_t *game_state);
 void setup_window();
