b3f7f5b38486e6170dbfec6d14332347ad9d3f56
Matthew John <matthew@dockstudios.co.uk>
2022-01-06 06:52:34 +0000

    Issue #72 Fix incorrect positive number being shown in show_signed_number.
    pos_value (which is passed to show_number) was not being set if the value
    variable was set to a positive number.
    -e 
    Original/build commit ID: 83a4d7def9dc708f2a40942c01fbdbd3d3ea4b87

--- window.c
+++ window.c
@@ -74,6 +74,8 @@ void show_signed_number(UINT8 start_x, UINT8 start_y, UINT8 max_digits, INT8 val
         is_negative = 1U;
         pos_value = 0U - value;
     }
+    else
+        pos_value = value;
 
     // Show number using max_digits, except one, which will be used for negative symbol.
     start_x = show_number(start_x + 1U, start_y, max_digits - 1U, pos_value);
