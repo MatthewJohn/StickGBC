05a12c82045c3f728a65ac4950082c24f6769782
Matthew John <matthew@dockstudios.co.uk>
2022-01-09 17:37:44 +0000

    Issue #83 Initial changes to display opening screen.
    Create new opening_screen C/h file and add method to load opening screen.
    Create main.h to expose the set_background_tiles method and ROM switching
    preprocessor definitions.
    Update joy.h to only include definitions once.
    Add new opening_screen to Makefile.
    Update set_background_tiles to take a ROM bank that should be switched to
    before returning, which is called by opening_screen_loop in ROM bank 7
    -e 
    Original/build commit ID: 0dbf440f8c9b8adb44272d7f085f6fce5d1b4199

---
+++ opening_screen.c
@@ -0,0 +1,40 @@
+/*
+ * StickGBC by Matt Comben is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+ * To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by-nc-nd/4.0/.
+ */
+
+#pragma bank=7
+
+#include "opening_screen.h"
+#include "opening_screen_map.h"
+#include "opening_screen_palette.h"
+#include "opening_screen_tiles.h"
+#include "game_constants.h"
+#include "main.h"
+
+void opening_screen_loop(screen_state_t *screen_state, joypad_state_t *joypad_state)
+{
+    DISPLAY_OFF;
+
+    screen_state->background_color_palette = opening_screen_palette;
+    screen_state->background_tile_map = openingscreenmap;
+    screen_state->background_tiles = openingscreentiles;
+    screen_state->background_width = openingscreenmapWidth;
+
+    screen_state->draw_offset_x = 0U;
+    screen_state->draw_offset_y = 0U;
+    screen_state->draw_max_x = SCREEN_WIDTH_TILES;
+    screen_state->draw_max_y = SCREEN_HEIGHT_TILES;
+
+    set_background_tiles(ROM_BANK_OPENING_SCREEN, ROM_BANK_OPENING_SCREEN);
+
+    // Move background to top left
+    scroll_bkg(0, 0);
+
+    // Load additional tiles required
+    set_bkg_data(8U, 19U, &(openingscreentiles[8U << 4]));
+
+    DISPLAY_ON;
+//    delay(5000);
+}
---
+++ opening_screen.h
@@ -0,0 +1,16 @@
+/*
+ * StickGBC by Matt Comben is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+ * To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by-nc-nd/4.0/.
+ */
+
+
+#include "joy.h"
+#include "screen_state.c"
+
+#ifndef OPENING_SCREEN_DEF
+#define OPENING_SCREEN_DEF
+
+void opening_screen_loop(screen_state_t *screen_state, joypad_state_t *joypad_state);
+
+#endif
