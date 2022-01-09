08066855140c07dc8d8c6604f4386d56abbf23fc
Matthew John <matthew@dockstudios.co.uk>
2022-01-09 08:40:01 +0000

    Issue #58 Add test to check for purchasing item with buffer underflow bug -e Original/build commit ID: 02b6e9edda8808f9084aaa9dd3075065f29e69c8

--- scripts/ci_tests.sh
+++ scripts/ci_tests.sh
@@ -29,14 +29,21 @@ mkdir test_{1..3}
 set +e
 docker run --rm -e INPUT_DIR=/app/test_1 -e OUTPUT_DIR=/output/test_1 -e ROM_DIR=/output -e ROM_FILE=main.gb -e DELAY=40 -e BGB_TIMEOUT=60 -v `pwd`:/output stickrpg-automated-test-tool:latest
 test_1_failed=$?
+
 docker run --rm -e INPUT_DIR=/app/test_2 -e OUTPUT_DIR=/output/test_2 -e ROM_DIR=/output -e ROM_FILE=main.gb -e DELAY=40 -e BGB_TIMEOUT=60 -v `pwd`:/output stickrpg-automated-test-tool:latest
 test_2_failed=$?
+
 docker run --rm -e INPUT_DIR=/app/test_3 -e OUTPUT_DIR=/output/test_3 -e ROM_DIR=/output -e ROM_FILE=main.gb -e DELAY=40 -e BGB_TIMEOUT=60 -v `pwd`:/output stickrpg-automated-test-tool:latest
 test_3_failed=$?
 
+docker run --rm -e INPUT_DIR=/app/test_4 -e OUTPUT_DIR=/output/test_4 -e ROM_DIR=/output -e ROM_FILE=main.gb -e DELAY=40 -e BGB_TIMEOUT=60 -v `pwd`:/output stickrpg-automated-test-tool:latest
+test_4_failed=$?
+
+
+
 set -e
 
-if [ "$test_1_failed" != "0" ] || [ "$test_1_failed" != "0" ] || [ "$test_1_failed" != "0" ]
+if [ "$test_1_failed" != "0" ] || [ "$test_1_failed" != "0" ] || [ "$test_1_failed" != "0" ] || [ "$test_4_failed" != "0" ]
 then
   echo One or more tests failed
   exit 1
