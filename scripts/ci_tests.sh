31e7b60c896116c287625e760c8c80a1011dddbe
Matthew John <matthew@dockstudios.co.uk>
2022-01-09 08:38:18 +0000

    Issue #50 Add script to run CI tests -e Original/build commit ID: 08df3ea1268c2aebc6c4c6f76066fd8c89a731ed

---
+++ scripts/ci_tests.sh
@@ -0,0 +1,44 @@
+# StickGBC by Matt Comben is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+# To view a copy of this license, visit
+# http://creativecommons.org/licenses/by-nc-nd/4.0/.
+
+#!/bin/bash
+
+set -e
+set -x
+
+cd ./games/stickrpg
+
+
+# Obtain artifact
+# Attempt for 60 seconds
+for i in {1..60}
+do
+  { curl --output main.gb --fail http://pub-jenkins.dock.studios/artifacts/GameboyDev/StickGBC/Build_PR/$gitlabMergeRequestLastCommit/main.gb && break; } || true
+  sleep 1
+done
+
+if [ ! -f main.gb ]
+then
+  echo Unable to obtain artifact
+  exit 1
+fi
+
+mkdir test_{1..3}
+
+set +e
+docker run --rm -e INPUT_DIR=/app/test_1 -e OUTPUT_DIR=/output/test_1 -e ROM_DIR=/output -e ROM_FILE=main.gb -e DELAY=40 -e BGB_TIMEOUT=60 -v `pwd`:/output stickrpg-automated-test-tool:latest
+test_1_failed=$?
+docker run --rm -e INPUT_DIR=/app/test_2 -e OUTPUT_DIR=/output/test_2 -e ROM_DIR=/output -e ROM_FILE=main.gb -e DELAY=40 -e BGB_TIMEOUT=60 -v `pwd`:/output stickrpg-automated-test-tool:latest
+test_2_failed=$?
+docker run --rm -e INPUT_DIR=/app/test_3 -e OUTPUT_DIR=/output/test_3 -e ROM_DIR=/output -e ROM_FILE=main.gb -e DELAY=40 -e BGB_TIMEOUT=60 -v `pwd`:/output stickrpg-automated-test-tool:latest
+test_3_failed=$?
+
+set -e
+
+if [ "$test_1_failed" != "0" ] || [ "$test_1_failed" != "0" ] || [ "$test_1_failed" != "0" ]
+then
+  echo One or more tests failed
+  exit 1
+fi
+
