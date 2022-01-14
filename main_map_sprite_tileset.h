5968b5f203eb04ebaa3862bc96f86fc2ee40f53f
Matthew John <matthew@dockstudios.co.uk>
2022-01-14 07:24:48 +0000

    Issue #25 Re-assign car tiles.
    Update so that up-right-diagonal format is still used and tiles for back
    of car are after the 3 for front of car
    -e 
    Original/build commit ID: cb1d486e4d0ef9c89f8259a6361855b5ed301657

--- main_map_sprite_tileset.h
+++ main_map_sprite_tileset.h
@@ -16,7 +16,7 @@
   Compression          : None.
   Counter              : None.
   Tile size            : 8 x 8
-  Tiles                : 0 to 11
+  Tiles                : 0 to 13
 
   Palette colors       : Included.
   SGB Palette          : None.
