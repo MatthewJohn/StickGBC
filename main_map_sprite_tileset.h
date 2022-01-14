c64794de60fa6a305bd94bf0cef759e7316e35fe
Matthew John <matthew@dockstudios.co.uk>
2022-01-14 06:13:23 +0000

    Issue #25 Rotate 'top-facing' car and export all sprite tiles. -e Original/build commit ID: 6117d169c653b53c587ba119ae09256ebb29cde3

--- main_map_sprite_tileset.h
+++ main_map_sprite_tileset.h
@@ -16,7 +16,7 @@
   Compression          : None.
   Counter              : None.
   Tile size            : 8 x 8
-  Tiles                : 0 to 5
+  Tiles                : 0 to 11
 
   Palette colors       : Included.
   SGB Palette          : None.
