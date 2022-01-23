bb48a58907295ef5473071bd863fc2609eab6a19
Matthew John <matthew@dockstudios.co.uk>
2022-01-23 08:09:18 +0000

    Issue #17 Move functionality to bus purchases to new file.
    Issue #75
    -e 
    Original/build commit ID: df37424b3f3927651b8aa862fa19d8d8ceebcc2c

--- main.c
+++ main.c
@@ -17,6 +17,7 @@
 #include "main_map_palette.h"
 #include "main_map_boundaries.h"
 #include "main_game.h"
+#include "logic_functions.h"
 
 #include "building_menu_tiles.h"
 #include "building_menu_tiles_2.h"
@@ -1505,147 +1506,6 @@ void show_inventory_screen() NONBANKED
     ROM_BANK_RESET;
 }
 
-void bus_sell_goods()
-{
-    UINT8 cost;
-    UINT16 offer;
-
-    // Check time of day - must be midnight (morning - 0)
-    if (game_state.hour != 0)
-        return;
-
-    if (menu_state.current_item_x == 0U && menu_state.current_item_y == 1U)  // Brooklyn
-    {
-        cost = 115U;
-    }
-    else if (menu_state.current_item_x == 1U && menu_state.current_item_y == 1U)  // Chicago
-    {
-        cost = 115U;
-    }
-    else if (menu_state.current_item_x == 0U && menu_state.current_item_y == 2U)  // Detroit
-    {
-        cost = 100U;
-    }
-    else if (menu_state.current_item_x == 1U && menu_state.current_item_y == 2U)  // Camden
-    {
-        cost = 130U;
-    }
-    else if (menu_state.current_item_x == 0U && menu_state.current_item_y == 3U)  // LA
-    {
-        cost = 100U;
-    }
-    else if (menu_state.current_item_x == 1U && menu_state.current_item_y == 3U)  // Vegas
-    {
-        cost = 130U;
-    }
-
-    // Check if user has enough money
-    if (! HAS_MONEY(cost))
-    {
-        return;
-    }
-    game_state.balance -= cost;
-
-    // If player hasn't got any booze or cocaine, leave
-    if (game_state.inventory[S_INVENTORY_COCAINE] == 0 && game_state.inventory[S_INVENTORY_BOTTLE_OF_BEER] == 0)
-        return;
-
-    // If player hasn't got a gun or ammo, lose booze, cocaine and money
-    if (game_state.inventory[S_INVENTORY_HAND_GUN] == 0U || game_state.inventory[S_INVENTORY_AMMO] == 0U)
-    {
-        game_state.balance = 0;
-        game_state.inventory[S_INVENTORY_COCAINE] = 0;
-        game_state.inventory[S_INVENTORY_BOTTLE_OF_BEER] = 0;
-        return;
-    }
-
-    // If strength is lower than random number between 100-225...
-    if ((100 + (sys_time % 125)) > game_state.strength)
-    {
-        // Lose cocaine, booze and money
-        game_state.balance = 0;
-        game_state.inventory[S_INVENTORY_COCAINE] = 0;
-        game_state.inventory[S_INVENTORY_BOTTLE_OF_BEER] = 0;
-        return;
-    }
-
-    // If cell phone not owned, leave
-    if (game_state.inventory[S_INVENTORY_CELL_PHONE] == 0U)
-        return;
-
-    // If player has more than 50 cocaine OR 50 booze, 'go to jail' for 5 days, losing gun, ammo, cocaine, booze
-    if (game_state.inventory[S_INVENTORY_COCAINE] > 50 || game_state.inventory[S_INVENTORY_BOTTLE_OF_BEER] > 50)
-    {
-        game_state.days_passed += 5U;
-        game_state.inventory[S_INVENTORY_COCAINE] = 0;
-        game_state.inventory[S_INVENTORY_BOTTLE_OF_BEER] = 0;
-        game_state.inventory[S_INVENTORY_HAND_GUN] = 0;
-        game_state.inventory[S_INVENTORY_AMMO] = 0;
-    }
-
-    // 1 in 10 chance that contact takes stash
-    if (sys_time % 10 == 0U)
-    {
-        game_state.inventory[S_INVENTORY_COCAINE] = 0;
-        game_state.inventory[S_INVENTORY_BOTTLE_OF_BEER] = 0;
-        return;
-    }
-
-    // SELL!
-
-    // Half chance of selling cocaine or booze
-    if (sys_time % 1 == 0U)
-    {
-        // Selling booze
-        offer = game_state.charm / 6U;
-
-        if (offer > 50U)
-            offer = 50U;
-
-        // Randomly decide whether to add/remove a small amount
-        if (sys_time % 1 == 0U)
-        {
-            offer += (sys_time % 5);
-        }
-        else if (offer > 5U)
-        {
-            offer -= (sys_time % 5);
-        }
-
-        // Minimum offer of 5
-        if (offer < 5U)
-            offer = 5U;
-
-        game_state.balance += (game_state.inventory[S_INVENTORY_BOTTLE_OF_BEER] * offer);
-        game_state.inventory[S_INVENTORY_BOTTLE_OF_BEER] = 0U;
-    }
-    else
-    {
-        // Selling cocaine
-        offer = 10 * (game_state.charm / 5U);
-
-        if (offer > 600U)
-            offer = 600U;
-
-        // Randomly decide whether to add/remove a small amount
-        if (sys_time % 1 == 0U)
-        {
-            offer += (sys_time % 50);
-        }
-        else if (offer > 50U)
-        {
-            offer -= (sys_time % 50);
-        }
-
-        // Minimum offer of 5
-        if (offer < 50U)
-            offer = 50U;
-
-        game_state.balance += (game_state.inventory[S_INVENTORY_COCAINE] * offer);
-        game_state.inventory[S_INVENTORY_COCAINE] = 0U;
-    }
-}
-
 // Called per cycle to update background position and sprite
 void update_state()
 {
@@ -2211,7 +2071,11 @@ void update_state()
                 // If not selecting from top row ('sell goods' or exit),
                 // perform sell_goods_city
                 if (menu_state.current_item_y != 0U)
-                    bus_sell_goods();
+                {
+                    ROM_BANK_LOGIC_FUNCTIONS_SWITCH;
+                    bus_sell_goods(&menu_state, &game_state);
+                    ROM_BANK_RESET;
+                }
             }
         }
         else
