2c962718395e98c73b04aad7dea1c819f4da0d79
Matthew John <matthew@dockstudios.co.uk>
2022-01-25 05:27:36 +0000

    Issue #104 Move functionality to process menus for restaurant, house and pawn to alternative ROM bank
    Issue #75
    -e 
    Original/build commit ID: 37177d87415ea213e1fccfc3e9e3244c5b2c9ed4

--- main.h
+++ main.h
@@ -45,6 +45,10 @@ UINT8 main_show_number(UINT8 start_x, UINT8 start_y, UINT8 max_digits, unsigned 
 void setup_building_menu(UINT8 menu_number, unsigned int return_bank);
 void move_to_menu_item(UINT8 new_x, UINT8 new_y, unsigned int return_bank);
 void modify_karma(INT8 karma_change);
+void main_do_work(unsigned int pay_per_hour, unsigned int number_of_hours, unsigned int return_bank);
+void load_menu_tiles(unsigned int return_bank);
+void check_end_game();
+UINT8 increase_charm(UINT8 cost, UINT8 number_of_hours, UINT8 charm, unsigned int return_bank);
 
 extern screen_state_t screen_state;
 extern joypad_state_t joypad_state;
