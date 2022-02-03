dd429bf47effd54c9ab8a58fe0a6505a307beb69
Matthew John <matthew@dockstudios.co.uk>
2022-02-03 07:32:08 +0000

    Issue #14 Remove EOL whitespac3 -e Original/build commit ID: efdcf2e09122658ec351bb15a9d7a471d8b76b1a

--- logic_functions.c
+++ logic_functions.c
@@ -462,7 +462,7 @@ void process_house_menu()
         check_end_game();
 
         DISPLAY_OFF;
-        
+
         // Update bank amounts
         game_state.bank_balance += ((game_state.bank_balance * game_state.bank_rate) / 1000);
         game_state.loan += ((game_state.loan * game_state.bank_rate) / 1000);
