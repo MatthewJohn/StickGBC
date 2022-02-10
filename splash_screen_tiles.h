038dd10ff80631eaa924fced621d0c90e37c8108
Matthew John <matthew@dockstudios.co.uk>
2022-02-10 07:26:29 +0000

    Issue #3 Export all tiles from splash screen -e Original/build commit ID: d04aa3c8f2eaabb9cc651217e2f56d9a2a47e415

--- splash_screen_tiles.h
+++ splash_screen_tiles.h
@@ -16,7 +16,7 @@
   Compression          : None.
   Counter              : None.
   Tile size            : 8 x 8
-  Tiles                : 0 to 127
+  Tiles                : 0 to 255
 
   Palette colors       : None.
   SGB Palette          : None.
