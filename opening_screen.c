7d0c66a8b037d0bebb1868851d2ae82a284204a6
Matthew John <matthew@dockstudios.co.uk>
2022-02-10 19:28:17 +0000

    Issue #3 Fix loading of all splash tiles in second bank -e Original/build commit ID: 467c4e6f6f9eac6353aa37a5a8e37b242e858836

--- opening_screen.c
+++ opening_screen.c
@@ -41,7 +41,7 @@ void splash_screen_loop()
     // Load additional tiles required
     set_bkg_data(8U, 120U, &(splashscreentiles[8U << 4]));
     VBK_REG = 1;
-    set_bkg_data(0U, 22U, &(splashscreentiles[128U << 4]));
+    set_bkg_data(0U, 25U, &(splashscreentiles[128U << 4]));
     VBK_REG = 0;
 
     DISPLAY_ON;
