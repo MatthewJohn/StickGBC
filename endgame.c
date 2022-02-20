edf5ec49decb7eb64b6ae16662626e87dc535349
Matthew John <matthew@dockstudios.co.uk>
2022-02-20 11:34:41 +0000

    Issue #115 Move end_game function to endgame.c and load endgame map/tiles -e Original/build commit ID: 1809c376626c14e495ebfa15cdd7f5e9988c85d9

---
+++ endgame.c
@@ -0,0 +1,50 @@
+/*
+ * StickGBC by Matt Comben is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+ * To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by-nc-nd/4.0/.
+ */
+
+#pragma bank=7
+
+#include "endgame_map.h"
+#include "endgame_tiles.h"
+#include "endgame_palette.h"
+#include "game_constants.h"
+#include "endgame.h"
+#include "main.h"
+
+void endgame()
+{
+    DISPLAY_OFF;
+
+    screen_state.background_color_palette = endgame_palette;
+    screen_state.background_tile_map = endgamemap;
+    screen_state.background_tiles = endgametiles;
+    screen_state.background_width = endgamemapWidth;
+
+    screen_state.draw_offset_x = 0U;
+    screen_state.draw_offset_y = 0U;
+    screen_state.draw_max_x = SCREEN_WIDTH_TILES;
+    screen_state.draw_max_y = SCREEN_HEIGHT_TILES;
+
+    set_background_tiles(
+        ROM_BANK_OPENING_SCREEN,  // Load tiles from this ROM bank
+        ROM_BANK_OPENING_SCREEN  // Return to the ROM bank for this function
+    );
+
+    // Move background to top left
+    scroll_bkg(0, 0);
+
+    // Load additional tiles required
+    set_bkg_data(8U, 86U, &(endgametiles[8U << 4]));
+
+    DISPLAY_ON;
+
+    // Wait for user to press A or START
+    while (joypad_state.a_pressed == 0U && joypad_state.start_pressed == 0U)
+    {
+        wait_vbl_done();
+
+        main_check_joy(ROM_BANK_OPENING_SCREEN);
+    }
+}
