da08bd7c76f76b55b61feb4eea8c04b37a291422
Matthew John <matthew@dockstudios.co.uk>
2023-04-02 12:58:55 +0100

    Update logic to use all bytes from source tile data, but mask each byte with the side of the tile
    Issue #43
    -e 
    Original/build commit ID: 40ebecee580c123ca5573f671c36e5551ac7ef2c

--- bar_fight.c
+++ bar_fight.c
@@ -26,15 +26,20 @@ UINT8 add_number(UINT8 tile_x, UINT8 tile_y, UINT16 number)
     UINT8 current_map_index;
     unsigned char tile_to_insert[0xFU];
     UINT8 source_tile_offset;
-    UINT8 source_tile_data_offset;
-    UINT8 destination_tile_offset;
+    UINT8 source_data_mask;
+    UINT8 destination_data_mask;
     unsigned char map_reference_data[0x2U];
     UINT8 byte_itx;
+    
+    for (byte_itx = 0; byte_itx < 16U; byte_itx ++)
+    {
+      tile_to_insert[byte_itx] = 0U;
+    }
+    destination_data_mask == 0xF0U;
 
     digit_count = 0;
     current_map_index = BAR_FIGHT_TILE_SCRATCH;
     overflow = number;
-    destination_tile_offset = 0U;
  
    // Iterate over number to display, until number is reduced to single digit
    // Use has_run to handle showing 0
@@ -49,19 +54,25 @@ UINT8 add_number(UINT8 tile_x, UINT8 tile_y, UINT16 number)
         // Each line in tile is made of 2 chars, meaing each line of a number is 1 byte.
         // Offset for first number (0, 2, etc.) is 0 (start of tile) and odd numbers are on the right
         // hand side, so each line offset is 1
-        source_tile_data_offset = (remainder % 2U);
-
-        // Iterate through each line, copying the byte to the tile data to insert.
-        for (byte_itx = 0; byte_itx < 8U; byte_itx ++) {
-            source_tile_index = BAR_FIGHT_NUMERIC_TILE_START + source_tile_offset;
-            source_tile_index = source_tile_index << 4;
-            source_tile_index += (byte_itx * 2U) + source_tile_data_offset;
-            // @TODO Temporary hack to show character
-            source_tile_offset = 1024 + (byte_itx * 2);
-            tile_to_insert[(byte_itx * 2U) + destination_tile_offset] = barfighttiles[source_tile_index];
+        if (remainder % 2U == 0U) {
+            source_data_mask = 0xF0U;
+        } else {
+            source_data_mask = 0x0FU;
         }
 
-        set_bkg_data(current_map_index, 1U, &(tile_to_insert[0U]));
+        // Iterate through each line, copying the byte to the tile data to insert.
+        for (byte_itx = 0; byte_itx < 16U; byte_itx ++) {
+            source_tile_index = BAR_FIGHT_NUMERIC_TILE_START + source_tile_offset;
+            source_tile_index = source_tile_index << 4;
+            source_tile_index += byte_itx;
+            // @TODO Temporary hack to show character
+            //source_tile_offset = 1024 + byte_itx;
+            // Remove the currently character side of destination data
+            //tile_to_insert[byte_itx] = (tile_to_insert[byte_itx] & (destination_data_mask ^ 0xFFU));
+            tile_to_insert[byte_itx] |= (barfighttiles[source_tile_index] & source_data_mask);
+        }
+
+        set_bkg_data(current_map_index, 2U, &(tile_to_insert[0U]));
         
         // Create map data for tile
         map_reference_data[0U] = current_map_index;
@@ -73,10 +84,15 @@ UINT8 add_number(UINT8 tile_x, UINT8 tile_y, UINT16 number)
         }
         
         // Flip the destination file offset on each number
-        if (destination_tile_offset == 0U) {
-            destination_tile_offset = 1U;
+        if (destination_data_mask == 0xF0U) {
+            destination_data_mask = 0x0FU;
         } else {
-            destination_tile_offset = 0U;
+            destination_data_mask = 0xF0U;
+            // Reset file data
+            for (byte_itx = 0; byte_itx < 16U; byte_itx ++)
+            {
+              tile_to_insert[byte_itx] = 0U;
+            }
         }
     }
 
