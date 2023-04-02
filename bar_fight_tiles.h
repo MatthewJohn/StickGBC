390633ce3aa51ebebec613a0b6cdaab8f5f41690
Matthew John <matthew@dockstudios.co.uk>
2023-04-02 11:07:24 +0100

    Add numeric digits to bar fight tiles
    Issue #43
    -e 
    Original/build commit ID: 8febd6f8325142f475add06608e9989cc0b527c7

--- bar_fight_tiles.h
+++ bar_fight_tiles.h
@@ -16,7 +16,7 @@
   Compression          : None.
   Counter              : None.
   Tile size            : 8 x 8
-  Tiles                : 0 to 62
+  Tiles                : 0 to 68
 
   Palette colors       : None.
   SGB Palette          : None.
