1b311809f0ba6030195c81ba14a1ad0fe3cb873f
Matthew John <matthew@dockstudios.co.uk>
2023-04-02 11:48:04 +0100

    Initial draft changes to generate number tiles for bar fight
    Issue #43
    -e 
    Original/build commit ID: 303c1048b71c9c45983d47278fdfabf5eceae2ef

--- bar_fight.c
+++ bar_fight.c
@@ -13,6 +13,63 @@
 #include "bar_fight.h"
 #include "main.h"
 
+#define BAR_FIGHT_TILE_SCRATCH 69U
+#define BAR_FIGHT_NUMERIC_TILE_START 63U
+
+UINT8 add_number(UINT8 tile_x, UINT8 tile_y, UINT16 number)
+{
+    UINT8 digit_count;
+    UINT16 overflow;
+    UINT8 remainder;
+    UINT16 multiple;
+    BOOLEAN has_run;
+    UINT8 current_map_index;
+    unsigned char tile_to_insert[0xFU];
+    UINT8 source_tile_offset;
+    UINT8 source_tile_data_offset;
+    UINT8 destination_tile_offset;
+    UINT8 byte_itx;
+
+    digit_count = 0;
+    current_map_index = BAR_FIGHT_TILE_SCRATCH;
+    overflow = number;
+    has_run = 0U;
+    destination_tile_offset = 0U;
+ 
+   // Iterate over number to display, until number is reduced to single digit
+   // Use has_run to handle showing 0
+    while (overflow > 0 || has_run == 0U) {
+        digit_count ++;
+        remainder = overflow % 10U;
+        overflow = overflow / 10U;
+
+        // Each source tile contains 2 numbers,
+        // so source tile is tile main offset with an offset of number to use / 2
+        source_tile_offset = remainder / 2U;
+        // Each line in tile is made of 2 chars, meaing each line of a number is 1 byte.
+        // Offset for first number (0, 2, etc.) is 0 (start of tile) and odd numbers are on the right
+        // hand side, so each line offset is 1
+        source_tile_data_offset = (remainder % 2U);
+
+        // Flip the destination file offset on each number
+        if (destination_tile_offset == 0U) {
+            destination_tile_offset = 1U;
+        } else {
+            destination_tile_offset = 0U;
+        }
+
+        // Iterate through each line, copying the byte to the tile data to insert.
+        for (byte_itx = 0; byte_itx <= 8U; byte_itx ++) {
+            tile_to_insert[destination_tile_offset + byte_itx] = barfighttiles[BAR_FIGHT_NUMERIC_TILE_START + source_tile_offset + source_tile_data_offset];
+        }
+
+        set_bkg_data(current_map_index, 1U, &(tile_to_insert[0U]));
+        has_run = 1U;
+    }
+
+    return current_map_index;
+}
+
 void enter_bar_fight()
 {
 
@@ -43,11 +100,12 @@ void enter_bar_fight()
     move_bkg(0, 0);
 
     // Load additional tiles required
-    set_bkg_data(8U, 55U, &(barfighttiles[8U << 4]));
+    set_bkg_data(8U, 60U, &(barfighttiles[8U << 4]));
 
     DISPLAY_ON;
 
     // Wait for user to press A or START
+    add_number(2U, 12U, 8U);
     joypad_state.a_pressed = 0U;
     joypad_state.start_pressed = 0U;
     while (joypad_state.a_pressed == 0U && joypad_state.start_pressed == 0U)
