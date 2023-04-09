035d5224796ce800d2b256a421a69c6101969d86
Matthew John <matthew@dockstudios.co.uk>
2023-04-09 08:43:01 +0100

    Export additional tiles for appliance store
    Issue #84
    -e 
    Original/build commit ID: 599307cfecf0cd90fdad2ea95fa62ca695044052

--- main_map_tileset.h
+++ main_map_tileset.h
@@ -16,7 +16,7 @@
   Compression          : None.
   Counter              : None.
   Tile size            : 8 x 8
-  Tiles                : 0 to 66
+  Tiles                : 0 to 77
 
   Palette colors       : None.
   SGB Palette          : None.
